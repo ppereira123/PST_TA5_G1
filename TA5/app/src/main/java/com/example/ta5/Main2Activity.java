c29533f0d6e10fee478c1936e6aa74e40ac4889b
jercleon <jercleon@espol.edu.ec>
2019-07-23 18:55:58 -0500

    reproduccion de video de youtube con videoView

--- TA5/app/src/main/java/com/example/ta5/Main2Activity.java
+++ TA5/app/src/main/java/com/example/ta5/Main2Activity.java
@@ -16,7 +16,8 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         VideoView mVideoView =(VideoView)findViewById(R.id.videoView);
-        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.jeje;
+        //String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.jeje;
+        String videoPath = "rtsp://r6---sn-q4fl6nle.googlevideo.com/Cj0LENy73wIaNAlVLiTknYIT-hMYDSANFC18nTddMOCoAUIASARgtN7DxJSlrNFcigELMW1UM2lhTnE3NlkM/D3DE35780F4BBB47C3094806AB566E747686D818.E5774AD80B220D2A7386CCFCF5E4D452027E4468/yt8/1/video.3gp";
         Uri uri = Uri.parse(videoPath);
         mVideoView.setVideoURI(uri);
         MediaController mediaC = new MediaController(this);
