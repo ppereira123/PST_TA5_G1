c5c1fe5d602702fbd0d7fd3a93f6fd93559d42be
Cacarden10 <cacarden@espol.edu.ec>
2019-07-24 00:11:48 -0500

    Cambios en la aplicacion-Mapa de google
    Creación del mapa de google, usando el api de google maps, creedenciales de google, se sube recien por los problemas de consola, en branch se puede observar que ya se habia subido, solo que esta vez si a la rama master. por Carlos Cardenas

--- TA5/app/src/main/java/com/example/ta5/MainActivity.java
+++ TA5/app/src/main/java/com/example/ta5/MainActivity.java
@@ -23,4 +23,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+    public void mapa(View view) {
+        Intent i = new Intent(this, Mapa.class );
+        startActivity(i);
+    }
+
 }
